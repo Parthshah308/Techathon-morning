19186c935c9f18073f17c5ca9d8587bc833c6a62
parthshah308 <shahparth3082003@gmail.com>
2022-09-25 16:22:00 +0530

    dashboard change

--- pages/Dashboard/index.js
+++ pages/Dashboard/index.js
@@ -126,43 +126,7 @@ const Dashboard = () => {
               </div>
             </div>
           </div>
-          <div class="col-xl-6 col-lg-6">
-            <div class="card l-bg-orange-dark">
-              <div class="card-statistic-3 p-4">
-                <div class="card-icon card-icon-large">
-                  <i class="fas fa-dollar-sign"></i>
-                </div>
-                <div class="mb-4">
-                  <h5 class="card-title mb-0">Pending Requests</h5>
-                </div>
-                <div class="row align-items-center mb-2 d-flex">
-                  <div class="col-8">
-                    <h2 class="d-flex align-items-center mb-0">$11.61k</h2>
-                  </div>
-                  <div class="col-4 text-right">
-                    <span>
-                      2.5% <i class="fa fa-arrow-up"></i>
-                    </span>
-                  </div>
-                </div>
-                <div
-                  class="progress mt-1 "
-                  data-height="8"
-                  style={{ height: '8px' }}
-                >
-                  <div
-                    class="progress-bar l-bg-cyan"
-                    role="progressbar"
-                    data-width="25%"
-                    aria-valuenow="25"
-                    aria-valuemin="0"
-                    aria-valuemax="100"
-                    style={{ width: '25%' }}
-                  ></div>
-                </div>
-              </div>
-            </div>
-          </div>
+           
         </div>
       </div>
     </Layout>
